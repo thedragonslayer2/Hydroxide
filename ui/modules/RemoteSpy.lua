b97e55a0d9dafb9203cdf9263e68ea5dcdd94579
thedragonslayer2 <84556593+thedragonslayer2@users.noreply.github.com>
2024-02-08 13:45:49 +0800

    Update RemoteSpy.lua

--- ui/modules/RemoteSpy.lua
+++ ui/modules/RemoteSpy.lua
@@ -405,7 +405,6 @@ function ArgsLog.new(log, callInfo)
     end
 
     button:SetRightCallback(function()
-        print"button:SetRightCallback called"
         selected.args = callInfo.args
         selected.callingScript = callInfo.script
         selected.func = callInfo.func
